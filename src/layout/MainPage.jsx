9d7b128663f273afdc5bde486ebd6206c5801ba7
fahima <sfahima1388@gmail.com>
2024-08-02 18:02:12 +0600

    customized colors added as tailwind class

--- src/layout/MainPage.jsx
+++ src/layout/MainPage.jsx
@@ -1,10 +1,12 @@
+import { Outlet } from "react-router-dom";
+
 const MainPage = () => {
   return (
     <div>
       <h1 className="text-center text-3xl">Hello World!</h1>
       <h1 className="text-center text-3xl font-semibold">Hello World!</h1>
       <h1 className="text-center text-3xl font-bold">Hello World!</h1>
-      
+      <Outlet />
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: { colors: {
-      
-    } },
+    extend: {
+      colors: {
+        blue: "#156BCA",
+        darkGray: "#1F2833",
+        gray: "#5C635A",
+        lightBlue: "#D4E9FF",
+        red: "#F15E4A",
+      },
+    },
   },
   plugins: [],
 };
