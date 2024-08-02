d76c56c952f81dae3eee6e4e5b8102791079b6b6
fahima <sfahima1388@gmail.com>
2024-08-02 17:56:12 +0600

    initial setup

---
+++ src/layout/MainPage.jsx
@@ -0,0 +1,12 @@
+const MainPage = () => {
+  return (
+    <div>
+      <h1 className="text-center text-3xl">Hello World!</h1>
+      <h1 className="text-center text-3xl font-semibold">Hello World!</h1>
+      <h1 className="text-center text-3xl font-bold">Hello World!</h1>
+      
+    </div>
+  );
+};
+
+export default MainPage;
