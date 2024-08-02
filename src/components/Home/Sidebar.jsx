26b92e09e16f7e740942833c4f54d0ea65f815e5
fahima <sfahima1388@gmail.com>
2024-08-02 20:51:53 +0600

    sidebar for smaller device added

--- src/components/Home/Sidebar.jsx
+++ src/components/Home/Sidebar.jsx
@@ -4,14 +4,19 @@ import { LuSettings, LuUsers } from "react-icons/lu";
 import { TbFileDescription, TbInfoSquare } from "react-icons/tb";
 import { Link } from "react-router-dom";
 
-const Sidebar = () => {
+const Sidebar = ({ isOpen, toggleSidebar }) => {
   return (
-    <>
-      <nav className="w-48 flex flex-col gap-3 text-center py-5">
+    <div
+      className={`fixed z-50 top-0 right-0  bg-white transform ${
+        isOpen ? "translate-x-0" : "translate-x-full"
+      } transition-transform duration-300 lg:translate-x-0 w-48 lg:relative lg:top-auto lg:right-auto lg:h-auto`}
+    >
+      <nav className="flex flex-col gap-3 text-center py-5 h-screen border-r border-r-[#E7E7E7]">
         <h1 className="text-[#4285F3] text-4xl p-5 uppercase">Logo</h1>
         <Link
           to="/"
           className="text-base hover:bg-lightBlue text-gray hover:text-black hover:font-medium p-2 inline-flex gap-2 items-center hover:border-l-[3px] hover:border-[#102C4A]"
+          onClick={toggleSidebar}
         >
           <GrAppsRounded />
           Home
@@ -19,6 +24,7 @@ const Sidebar = () => {
         <Link
           to="/listing"
           className="text-base hover:bg-lightBlue text-gray hover:text-black hover:font-medium p-2 inline-flex gap-2 items-center hover:border-l-[3px] hover:border-[#102C4A]"
+          onClick={toggleSidebar}
         >
           <LuUsers />
           New Listing
@@ -26,6 +32,7 @@ const Sidebar = () => {
         <Link
           to="/search"
           className="text-base hover:bg-lightBlue text-gray hover:text-black hover:font-medium p-2 inline-flex gap-2 items-center hover:border-l-[3px] hover:border-[#102C4A]"
+          onClick={toggleSidebar}
         >
           <CiSearch />
           Search
@@ -33,34 +40,38 @@ const Sidebar = () => {
         <Link
           to="/about"
           className="text-base hover:bg-lightBlue text-gray hover:text-black hover:font-medium p-2 inline-flex gap-2 items-center hover:border-l-[3px] hover:border-[#102C4A]"
+          onClick={toggleSidebar}
         >
           <TbFileDescription />
           About
         </Link>
         <Link
-          to="/about"
+          to="/favorites"
           className="text-base hover:bg-lightBlue text-gray hover:text-black hover:font-medium p-2 inline-flex gap-2 items-center hover:border-l-[3px] hover:border-[#102C4A]"
+          onClick={toggleSidebar}
         >
           <CiHeart />
           Favorites
         </Link>
         <hr className="text-[#E7E7E7]" />
         <Link
-          to="/about"
+          to="/help"
           className="text-base hover:bg-lightBlue text-gray hover:text-black hover:font-medium p-2 inline-flex gap-2 items-center hover:border-l-[3px] hover:border-[#102C4A]"
+          onClick={toggleSidebar}
         >
           <TbInfoSquare />
           Help Center
         </Link>
         <Link
-          to="/about"
+          to="/settings"
           className="text-base hover:bg-lightBlue text-gray hover:text-black hover:font-medium p-2 inline-flex gap-2 items-center hover:border-l-[3px] hover:border-[#102C4A]"
+          onClick={toggleSidebar}
         >
           <LuSettings />
           Settings
         </Link>
       </nav>
-    </>
+    </div>
   );
 };
 
--- src/layout/MainPage.jsx
+++ src/layout/MainPage.jsx
@@ -1,18 +1,54 @@
+import { useState, useRef, useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import Sidebar from "../components/Home/Sidebar";
+import { HiOutlineMenuAlt3 } from "react-icons/hi";
 
 const MainPage = () => {
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
+  const sidebarRef = useRef(null);
+
+  const toggleSidebar = () => {
+    // console.log("Sidebar toggled");
+    setSidebarOpen(!isSidebarOpen);
+  };
+
+  const handleClickOutside = (event) => {
+    if (sidebarRef.current && !sidebarRef.current.contains(event.target)) {
+      setSidebarOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    if (isSidebarOpen) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isSidebarOpen]);
+
   return (
-    <div>
-      <div className="flex">
-        <div className="h-screen fixed z-[99999] shadow-xl">
-          <div className="hidden lg:flex flex-col max-w-64 mx-auto">
-            <Sidebar />
-          </div>
-        </div>
-        <div className="md:ml-56">
-          <Outlet />
-        </div>
+    <div className="relative flex">
+      <div className="lg:hidden">
+        <button
+          className="fixed z-50 top-5 right-5 text-3xl text-black"
+          onClick={toggleSidebar}
+        >
+          <HiOutlineMenuAlt3 />
+        </button>
+      </div>
+      <div ref={sidebarRef}>
+        <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
+      </div>
+      <div
+        className={`flex-1 transition-filter duration-300 ml-10 ${
+          isSidebarOpen ? "blur-sm" : "blur-none"
+        }`}
+      >
+        <Outlet />
       </div>
     </div>
   );
